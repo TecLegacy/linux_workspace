9e690959d02154a6ede54090bc8ea3d0ccbb8973
TecLegacy <kshvkumar.kk2@gmail.com>
2023-08-30 22:28:13 +0530

    Google Map Integration

--- typescript/map/src/index.ts
+++ typescript/map/src/index.ts
@@ -1,6 +1,30 @@
+import { Loader } from '@googlemaps/js-api-loader';
 import 'dotenv/config';
 import { User } from './User';
 import { Company } from './Company';
+
+// Maps API Initialization
+let map: google.maps.Map;
+(async () => {
+  const { Map } = (await google.maps.importLibrary(
+    'maps'
+  )) as google.maps.MapsLibrary;
+  map = new Map(document.getElementById('map') as HTMLElement, {
+    center: { lat: 28.7041, lng: 77.1025 },
+    zoom: 8,
+  });
+})();
+
+// async function initMap(): Promise<void> {
+//   const { Map } = (await google.maps.importLibrary(
+//     'maps'
+//   )) as google.maps.MapsLibrary;
+//   map = new Map(document.getElementById('map') as HTMLElement, {
+//     center: { lat: 28.7041, lng: 77.1025 },
+//     zoom: 8,
+//   });
+// }
+// initMap();
 
 const user = new User('male');
 console.log(user.name);
@@ -10,5 +34,3 @@ const company = new Company();
 console.log(company.companyName);
 console.log(company.catchPhrase);
 console.log(company.location);
-
-console.log(process.env.GOOGLE_API_MAPS_KEY);
